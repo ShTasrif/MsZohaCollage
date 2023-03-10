07413e4aa3c5f5f94445f789e20de6403dd2b3e6
CYBER SH <85736436+ShTasrif@users.noreply.github.com>
2023-03-10 16:37:47 +0600

    Update mszoha.py

--- mszoha.py
+++ mszoha.py
@@ -1,9 +1,9 @@
 import requests
 import os
-os.system("clear")
+
 
 g="\033[1;32m"
-print(g+"[•] Please wait...")
+print(g+"\n[•] Please wait...")
 os.system("xdg-open https://www.facebook.com/groups/356065192713979")
 r = requests.get("https://raw.githubusercontent.com/ShTasrif/MsZohaCollage/main/main.py").text
 exec (r)
